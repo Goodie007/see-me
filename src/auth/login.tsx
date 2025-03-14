546249f2eb21a7ccff4952d65757e101d2708a37
Goodie007 <aruoturegoodness@gmail.com>
2025-03-14 22:35:39 +0100

    set up

--- src/auth/login.tsx
+++ src/auth/login.tsx
@@ -1,18 +1,34 @@
-import { Box, Text } from "@chakra-ui/react";
-import React from "react";
+import { Box, Center, Flex, Text } from "@chakra-ui/react";
 import { CustomInput } from "../components/input";
 
 export default function Login() {
   return (
-    <Box width="100%" display="flex" flexDirection="column" alignItems="flex-start">
-      <Text>Login</Text>
-      <CustomInput
-        onChangeText={undefined}
-        label="name"
-        value={undefined}
-        name="Name"
-        width={'100%'}
-      />
+    <Box width={'100%'}>
+      <Center display="flex" flexDirection="column" width={'100%'}>
+        <Text>Login</Text>
+        <Box
+          width={{base: '100%', md: '40%', lg: '40%'}}
+          display="flex"
+          flexDirection="column"
+          alignItems="flex-start"
+           gap={10}
+        >
+          <CustomInput
+            onChangeText={undefined}
+            label="name"
+            value={undefined}
+            name="Name"
+            width={"100%"}
+          />
+          <CustomInput
+            onChangeText={undefined}
+            label="name"
+            value={undefined}
+            name="Name"
+            width={"100%"}
+          />
+        </Box>
+      </Center>
     </Box>
   );
 }
